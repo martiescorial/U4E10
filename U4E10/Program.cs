6ee58726ce7bdb7a17d6345665979111b7dd78f1
Martina <martinaescorial9@gmail.com>
2021-06-19 08:24:40 +0200

    Add project files.

---
+++ U4E10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace U4E10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 2;
+            while (i <= 100)
+            {
+                if (i % 2 == 0 || i % 3 == 0)
+                    Console.WriteLine(i);
+                i += 2;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
